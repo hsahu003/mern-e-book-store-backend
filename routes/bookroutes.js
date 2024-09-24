7ff5305a25b875caf95f2ce5916859f45d143ef0
Hemendra <hsahu003@gmail.com>
2024-09-24 13:22:35 +0530

    added apis for users

---
+++ routes/bookroutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+const Book = require('../model/bookSchema');
+const router = express.Router();
+
+module.exports = router;
